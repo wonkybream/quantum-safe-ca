4db6f31777d700abd986c6fcfe4ddf74d35099a3
wonkybream <sampo.l@protonmail.com>
2024-11-21 18:16:10 +0200

    Add few echo and check if folder exists

--- full-ca.sh
+++ full-ca.sh
@@ -13,11 +13,17 @@ echo "Using '$1' algorithm..."
 
 CA_NAME=$(grep '^.\{6\}$' /usr/share/dict/words | sort -R | head -1)
 
+if [ -d "$CA_NAME" ]; then
+    echo "Directory $CA_NAME already exists. Exiting."
+    exit 1
+fi
+
 mkdir $CA_NAME && cd $CA_NAME
 
 # Root certificate
 openssl genpkey -algorithm $1 -out root_key.pem
 openssl req -x509 -new -key root_key.pem -days 3650 -out root_cert.pem -subj "/CN=Root Certificate"
+echo "Root certificate created successfully"
 
 # Intermediate A and B certificates
 openssl genpkey -algorithm $1 -out intermediate_a_key.pem
@@ -28,6 +34,7 @@ openssl req -new -key intermediate_b_key.pem -out intermediate_b.csr -subj "/CN=
 
 openssl x509 -req -in intermediate_a.csr -CA root_cert.pem -CAkey root_key.pem -CAcreateserial -days 1825 -out intermediate_a_cert.pem
 openssl x509 -req -in intermediate_b.csr -CA root_cert.pem -CAkey root_key.pem -CAcreateserial -days 1825 -out intermediate_b_cert.pem
+echo "Intermediate A and B certificates created successfully"
 
 # Combine chains
 cat intermediate_a_cert.pem root_cert.pem > chain_a.pem
